20d79a21253a96b0d6f5e84b04617934d43e530e
GT <goto99@gmail.com>
2015-06-22 00:57:41 +0800

    makecachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,17 @@
 ## Programming assignment 2, lexical scoping
 ## Caching the inverse of a matrix
 
-## Write a short comment describing this function
-
+## function creates a special matrix which can cache its inverse providing that it's invertibal.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv = NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
